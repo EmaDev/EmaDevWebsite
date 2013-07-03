849341469f061f05d48087c361c45608fc69763f
Emanuele <pavanello.emanuele@gmail.com>
2013-07-03 09:59:15 +0200

    Initzialization project
    Creation of visual c++ project

---
+++ EmaDev/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char ** argv, char **envv)
+{
+
+	return 0;
+}
---
+++ Web/Web.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+namespace Web
+{
+	
+}
